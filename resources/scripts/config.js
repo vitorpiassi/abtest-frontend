f93e7016ee72c12770719d39780fcb9fcee11f6f
Gabriel Alan <gaba.alan@gmail.com>
2015-12-16 22:25:31 -0200

    Initial structure react + system

--- resources/scripts/config.js
+++ resources/scripts/config.js
@@ -1,17 +1,19 @@
 'use strict'
 
 System.config({
-	"baseURL": "dist",
-	"transpiler": "babel",
-	"babelOptions": {
-		"optional": [
+	baseURL: "dist/scripts",
+	defaultJSExtensions: true,
+	transpiler: "babel",
+	babelOptions: {
+		optional: [
 			"runtime"
 		]
 	},
-	"map": {
-		'React': 'depPath/react/react.min.js',
-		'Fluxxor': 'depPath/fluxxor/build/fluxxor.min.js',
+	map: {
+		'react': '../../../../bower_components/react/react.min.js',
+		'reactDom': '../../../../bower_components/react/react-dom.min.js',
+		'Fluxxor': '../../../../bower_components/fluxxor/build/fluxxor.min.js',
 	},
 });
 
-System.import("srcPath/scripts/helloworldcommonjs.js")
+System.import("main.js");

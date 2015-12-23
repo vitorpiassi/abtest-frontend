2398657cf5eb5660684b367a6160dfc986cfff99
Rodolfo dos Anjos <rodolfo.anjos@softexpert.com>
2015-12-23 18:06:19 -0200

    Started react experience

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,5 @@ var Flux = require("Flux"),
 	Index = require("views/Index.js"),
 	Home = require("views/pages/Home.js");
 
-debugger;
 Index.render();
 Home.render();
--- public/scripts/views/partials/NormalPage.js
+++ public/scripts/views/partials/NormalPage.js
@@ -4,21 +4,16 @@ var NormalPage = function () {
 	var React = require('react');
 	var ReactDOM = require('react-dom');
 	var rb = require('react-bootstrap');
+	var	PageHeader = rb.PageHeader;
 
-	var	PageHeader = rb.PageHeader,
-		ListGroup = rb.ListGroup,
-		ListGroupItem = rb.ListGroupItem;
+	var NormalList = require('./../widgets/NormalList');
 
 	var NormalPageObj = React.createClass({
 		render: function() {
 			return (
 				<div className="normal-page container">
 					<PageHeader>Hello World <small>React</small></PageHeader>
-					<ListGroup>
-						<ListGroupItem>Item 1</ListGroupItem>
-						<ListGroupItem>Item 2</ListGroupItem>
-						<ListGroupItem>...</ListGroupItem>
-					</ListGroup>
+					<NormalList></NormalList>
 				</div>
 			);
 		}
